57a72b16851ebbf2759ae9f9e2fa0698a7903a4a
abrah926 <abrahamvidalcastillo2@gmail.com>
2025-01-02 13:52:30 -0400

    bug fixed from apps.py

--- events/apps.py
+++ events/apps.py
@@ -1,12 +1,16 @@
 from django.apps import AppConfig
-from django_q.models import Schedule
-from events.recurrence import populate_future_events
 
 class EventsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'events'
 
-Schedule.objects.create(
-    func='events.recurrence.populate_future_events',
-    schedule_type=Schedule.WEEKLY,
-)
+    def ready(self):
+        # Import necessary models and functions here
+        from django_q.models import Schedule
+
+        # Check if the schedule already exists to avoid duplicates
+        if not Schedule.objects.filter(func='events.recurrence.populate_future_events').exists():
+            Schedule.objects.create(
+                func='events.recurrence.populate_future_events',
+                schedule_type=Schedule.WEEKLY,
+            )

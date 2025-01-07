f19ccf3b9f2f2f554652fc52e3aeaec65888da89
abrah926 <abrahamvidalcastillo2@gmail.com>
2025-01-07 18:56:29 -0400

    delay added to the nav bar for android

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 // services/api.js
 import axios from "axios";
 
-export const API_BASE_URL = "http://127.0.0.1:8000/api";
+export const API_BASE_URL = "https://salsa-backend.onrender.com/api";
+
 // Fetch all events
 export const fetchEvents = async () => {
     try {
